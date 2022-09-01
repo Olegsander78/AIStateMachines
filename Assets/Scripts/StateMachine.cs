5ba5195144cb05c8f128244419877a52b7730d55
Olegsander78 <onkotov@gmail.com>
2022-09-01 16:14:26 +0500

    Сделано - класс State Machine

--- Assets/Scripts/StateMachine.cs
+++ Assets/Scripts/StateMachine.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class StateMachine : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Stack<State> States { get; set; }
+
+    private void Awake()
     {
-        
+        States = new Stack<State>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (GetCurrentState() != null)
+        {
+            GetCurrentState().ActiveAction.Invoke();
+        }
+    }
+
+    public void PushState(Action active, Action onEnter, Action onExit)
+    {
+        if (GetCurrentState() != null)
+            GetCurrentState().OnExit();
+
+        State state = new State(active, onEnter, onExit);
+        States.Push(state);
+
+        GetCurrentState().OnEnter();
+    }
+
+    public void PopState()
+    {
+        if (GetCurrentState() != null)
+        {
+            GetCurrentState().OnExit();
+            GetCurrentState().ActiveAction = null;
+            States.Pop();
+            GetCurrentState().OnEnter();
+        }       
+    }
+    private State GetCurrentState()
+    {
+        return States.Count > 0 ? States.Peek() : null;
     }
 }
